c02469f62e013407605e29d8e06000fbd45e1c03
Amaan <amaan_baiyat@live.com>
2017-08-03 23:50:41 -0500

    Added document.ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,10 @@
-var sQuote ="";
-// Gets the first quote to display. Uses ajax to pull a quote and store
-// its quote content and the author
-$.ajax({
-  url:
-    "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-  success: function(a) {
-    document.getElementById("quote").innerHTML = a[0].content;
-    document.getElementById("author").innerHTML = "- " + a[0].title;
-    sQuote ="";
-    sQuote = a[0].content;
-  },
-  cache: false
-});
-
-// If the new quote button is clicked, gets a quote and displays it
-$("#newQ").on("click", function() {
+$(document).ready(function() {
+  var sQuote ="";
+  // Gets the first quote to display. Uses ajax to pull a quote and store
+  // its quote content and the author
   $.ajax({
     url:
-      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+      "https://cors-anywhere.herokuapp.com/https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
     success: function(a) {
       document.getElementById("quote").innerHTML = a[0].content;
       document.getElementById("author").innerHTML = "- " + a[0].title;
@@ -26,12 +13,27 @@ $("#newQ").on("click", function() {
     },
     cache: false
   });
-});
 
-// If the send tweet button is clicked, opens the new tweet url
-$("#sendTweet").on("click", function() {
-  if (jQuery(sQuote).text()) {
-    sQuote = jQuery(sQuote).text();
-  }
-  window.open("https://twitter.com/intent/tweet?text=" + sQuote);
-});
+  // If the new quote button is clicked, gets a quote and displays it
+  $("#newQ").on("click", function() {
+    $.ajax({
+      url:
+        "https://cors-anywhere.herokuapp.com/https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+      success: function(a) {
+        document.getElementById("quote").innerHTML = a[0].content;
+        document.getElementById("author").innerHTML = "- " + a[0].title;
+        sQuote ="";
+        sQuote = a[0].content;
+      },
+      cache: false
+    });
+  });
+
+  // If the send tweet button is clicked, opens the new tweet url
+  $("#sendTweet").on("click", function() {
+    if (jQuery(sQuote).text()) {
+      sQuote = jQuery(sQuote).text();
+    }
+    window.open("https://twitter.com/intent/tweet?text=" + sQuote);
+  });  
+})
